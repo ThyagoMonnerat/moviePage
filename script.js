0903b8d61912f4a51c9ac568ff422554b6c0f803
Thyago Monnerat <98675898+ThyagoMonnerat@users.noreply.github.com>
2022-07-07 22:45:37 -0300

    HTML, CSS and JS fixes
    HTML = Additions and Removals of Tags to Improve Experience
    CSS: color and proportion changes
    JS = functions for changing images and visual implementations.

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-const arrowButton = document.getElementById('arrowButton')
-const carousel = document.getElementById('movieCarousel')
+let arrowButton = document.getElementById('arrowButton')
+let carousel = document.getElementById('movieCarousel')
+let movieImgButton = document.querySelectorAll('.moveImg')
+let movieButton = document.querySelectorAll('.movie')
+let movieTitle = document.getElementById('movieTitle')
+let movieSinopse = document.getElementById('movieSinopse')
+let pageBg = document.querySelectorAll('.pageMovieBg')
 
-function slideMovies(){
-  carousel.scrollLeft += 200;
-
-  if(carousel.scrollLeft == 1512){
-    carousel.scrollLeft = 0
+function focusCheck() {
+  if (movieButton[0].autofocus == true) {
+    document.body.style = `background-image: url(${pageBg[0].src})`
+    movieTitle.innerText = movieButton[0].name
+    movieSinopse.innerText = movieButton[0].ariaLabel
   }
 }
+focusCheck()
 
+arrowButton.addEventListener('click', () => {
+  if (carousel.scrollLeft >= 0) {
+    carousel.scrollLeft += 300
+  }
+
+  if (carousel.scrollLeft >= 1400) {
+    carousel.scrollLeft = 0
+  }
+})
+
+movieImgButton.forEach(mI => {
+  mI.addEventListener('click', () => {})
+})
+
+movieButton.forEach(mN => {
+  mN.addEventListener('click', () => {
+    movieTitle.innerText = mN.name
+    movieSinopse.innerText = mN.ariaLabel
+    document.body.style = `background-image: url(${mN.lastElementChild.src})`
+  })
+})
